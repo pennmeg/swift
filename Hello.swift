c98402b5486fc4f173ee54810d5964550877f83c
Megan Penn <penn.meg@gmail.com>
2021-08-21 21:58:32 -0400

    First commit

---
+++ Hello.swift
@@ -0,0 +1,10 @@
+print("Hello World")
+
+// --- Swift Variables
+/* 
+Int: integer numbers
+Double: floating-point numbers
+String: a sequence of characters
+Bool: true/false values
+*/
+// Swift is a strongly typed language
